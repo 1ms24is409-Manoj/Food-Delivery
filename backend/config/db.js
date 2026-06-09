f9d5a7c566a61e2359956c8073336a39d0483af4
manoj-r-26 <YOUR_GITHUB_EMAIL>
2026-06-09 15:11:16 +0530

    change in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() =>console.log("DB Connected"));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
+
+export default connectDB;
